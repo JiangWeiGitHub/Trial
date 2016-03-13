ac51e47ae412fac93dd4d4fcbb6efe0a2ed1113a
root <root@debian>
2016-03-13 23:23:10 +0800

    added some files.

---
+++ DesignPattern/Singleton/SampleFour/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+class Singleton
+{
+public:
+	static Singleton *GetInstance()
+	{
+		static Singleton m_Instance;
+
+		return &m_Instance;
+	}
+
+	int GetTest()
+	{
+		return m_Test++;
+	}
+
+private:
+	Singleton(){ m_Test = 44; };
+	int m_Test;
+};
+
+int main(void)
+{
+	Singleton *singletonObj = Singleton ::GetInstance();
+	cout<<singletonObj->GetTest()<<endl;
+
+	singletonObj = Singleton ::GetInstance();
+	cout<<singletonObj->GetTest()<<endl;
+
+	return 0;
+}
