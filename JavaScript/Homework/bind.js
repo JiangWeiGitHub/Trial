7c33d0ae4fb260f1532782972eebaad6ada9c93d
jiangweigithub <wei.jiang@winsuntech.cn>
2016-12-11 21:58:49 -0800

    modified:   bind.js 	deleted:    lateBloomer.js

--- JavaScript/Homework/bind.js
+++ JavaScript/Homework/bind.js
@@ -1,5 +1,4 @@
-//bind
-
+// part 1
 global.x = 9
 
 const test = {
@@ -14,3 +13,28 @@ console.log(retrieveX())
 
 let boundGetX = retrieveX.bind(test)
 console.log(boundGetX())
+
+// part 2
+
+/* return 37 */
+// var root = { prop: 37 };
+
+// function getRoot() {
+//   return this.prop;
+// }
+
+// root.getAnother = { get: (getRoot).bind(root), prop: 42}
+
+// console.log(root.getAnother.get())
+
+
+/* return 42 */
+var root = { prop: 37 };
+
+function getRoot() {
+  return this.prop;
+}
+
+root.getAnother = { get: getRoot, prop: 42}
+
+console.log(root.getAnother.get())
