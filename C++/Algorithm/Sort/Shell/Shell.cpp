990303c5ac5cb148f7b94380697bccac95e53856
JiangWeiGitHub <wei.jiang@winsuntech.cn>
2017-07-06 18:54:25 +0800

    new file:   .vscode/settings.json 	modified:   Shell.cpp 	modified:   main.cpp

--- C++/Algorithm/Sort/Shell/Shell.cpp
+++ C++/Algorithm/Sort/Shell/Shell.cpp
@@ -1,38 +1,68 @@
 #include "./Shell.h"
 
-struct number
-{
-  int value;
-  struct number* before;
-  struct number* next;
-};
+// bool Shell::sort(int* number, int length)
+// {
+//   int i, j, gap;
+
+//   for (gap = length / 2; gap > 0; gap /= 2)
+//   {
+//     for (i = 0; i < gap; i++)
+//     {
+//       for (j = i + gap; j < length; j += gap)
+//       {
+//         if (number[j] > number[j - gap])
+//         {  
+//           int temp = number[j];
+//           int k = j - gap;
+
+//           while (k >= 0 && number[k] < temp)  
+//           {
+//             number[k + gap] = number[k];  
+//             k -= gap;
+//           }
+
+//           number[k + gap] = temp;  
+//         }
+//       }
+//     }
+//   }
+
+//   return true;
+// }
 
 bool Shell::sort(int* number, int length)
 {
-  int i, j, gap;
-
-  for (gap = length / 2; gap > 0; gap /= 2)
+  for(int i = length / 2; i >= 1; i = i / 2)
   {
-    for (i = 0; i < gap; i++)
+    std::cout<<">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> "<<std::endl;
+    std::cout<<"i: "<<i<<std::endl;
+    for(int j = 0; j < length / i; j++)
     {
-      for (j = i + gap; j < length; j += gap)
+      std::cout<<"j: "<<j<<std::endl;
+      for(int k = j; k < j + i; k++)
       {
-        if (number[j] > number[j - gap])
-        {  
-          int temp = number[j];
-          int k = j - gap;
+        std::cout<<"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"<<std::endl;
+        std::cout<<"k: "<<k<<std::endl;
+        if(number[k] > number[k + i])
+        {
+          std::cout<<"number[k]: "<<number[k]<<std::endl;
+          std::cout<<"number[k + i]: "<<number[k + i]<<std::endl;
+          std::cout<<"k: "<<k<<std::endl;
+          std::cout<<"k + i: "<<k + i<<std::endl;
 
-          while (k >= 0 && number[k] < temp)  
-          {
-            number[k + gap] = number[k];  
-            k -= gap;
-          }
+std::cout<<"--------------------------------------------------"<<std::endl;
 
-          number[k + gap] = temp;  
+          int tmp = number[k];
+          number[k] = number[k + i];
+          number[k + i] = tmp;
+          std::cout<<"k: "<<k<<std::endl;
+          std::cout<<"k + i: "<<k + i<<std::endl;
+          std::cout<<"number[k]: "<<number[k]<<std::endl;
+          std::cout<<"number[k + i]: "<<number[k + i]<<std::endl;
         }
       }
     }
-  }
 
-  return true;
+    std::cout<<"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"<<std::endl;
+  } 
 }
