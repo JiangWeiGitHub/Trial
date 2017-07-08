5501059002941118c070814bc0b89bff62f9e22e
JiangWeiGitHub <wei.jiang@winsuntech.cn>
2017-07-08 23:55:41 +0800

    modified:   C++/Algorithm/Sort/Shell/Shell.cpp 	modified:   C++/Algorithm/Sort/Shell/main.cpp

--- C++/Algorithm/Sort/Shell/Shell.cpp
+++ C++/Algorithm/Sort/Shell/Shell.cpp
@@ -1,68 +1,80 @@
 #include "./Shell.h"
 
-// bool Shell::sort(int* number, int length)
-// {
-//   int i, j, gap;
-
-//   for (gap = length / 2; gap > 0; gap /= 2)
-//   {
-//     for (i = 0; i < gap; i++)
-//     {
-//       for (j = i + gap; j < length; j += gap)
-//       {
-//         if (number[j] > number[j - gap])
-//         {  
-//           int temp = number[j];
-//           int k = j - gap;
-
-//           while (k >= 0 && number[k] < temp)  
-//           {
-//             number[k + gap] = number[k];  
-//             k -= gap;
-//           }
-
-//           number[k + gap] = temp;  
-//         }
-//       }
-//     }
-//   }
-
-//   return true;
-// }
-
 bool Shell::sort(int* number, int length)
 {
-  for(int i = length / 2; i >= 1; i = i / 2)
+  int i, j, gap;
+
+  for (gap = length / 2; gap > 0; gap /= 2)
   {
-    // std::cout<<">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> "<<std::endl;
-    // std::cout<<"i: "<<i<<std::endl;
-    for(int j = 0; j < length / i; j++)
+    for (i = 0; i < gap; i++)
     {
-      // std::cout<<"j: "<<j<<std::endl;
-      for(int k = j; k < j + i; k++)
+      for (j = i + gap; j < length; j += gap)
       {
-        // std::cout<<"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"<<std::endl;
-        // std::cout<<"k: "<<k<<std::endl;
-        if(number[k] > number[k + i])
-        {
-          // std::cout<<"number[k]: "<<number[k]<<std::endl;
-          // std::cout<<"number[k + i]: "<<number[k + i]<<std::endl;
-          // std::cout<<"k: "<<k<<std::endl;
-          // std::cout<<"k + i: "<<k + i<<std::endl;
+        if (number[j] > number[j - gap])
+        {  
+          int temp = number[j];
+          int k = j - gap;
 
-// std::cout<<"--------------------------------------------------"<<std::endl;
+          while (k >= 0 && number[k] < temp)  
+          {
+            number[k + gap] = number[k];  
+            k -= gap;
+          }
 
-          int tmp = number[k];
-          number[k] = number[k + i];
-          number[k + i] = tmp;
-          // std::cout<<"k: "<<k<<std::endl;
-          // std::cout<<"k + i: "<<k + i<<std::endl;
-          // std::cout<<"number[k]: "<<number[k]<<std::endl;
-          // std::cout<<"number[k + i]: "<<number[k + i]<<std::endl;
+          number[k + gap] = temp;  
         }
       }
     }
+  }
 
-    // std::cout<<"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"<<std::endl;
-  } 
+  return true;
 }
+
+// bool Shell::sort(int* number, int length)
+// {
+//   int left = 0;
+//   if(length % 2 == 0)
+//   {
+//     left = 0;
+//   }
+//   else
+//   {
+//     left = 1;
+//   }
+
+//   for(int i = length / 2; i >= 1; i = i / 2)
+//   {
+//     // std::cout<<">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> "<<std::endl;
+//     // std::cout<<"i: "<<i<<std::endl;
+
+//     for(int j = 0; j < i + left; j++)
+//     {
+//       // std::cout<<"j: "<<j<<std::endl;
+//       for(int k = j; k < j + i; k++)
+//       {
+//         // std::cout<<"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"<<std::endl;
+//         // std::cout<<"k: "<<k<<std::endl;
+//         // std::cout<<"j: "<<j<<std::endl;
+//         if(number[k] > number[k + i])
+//         {
+//           // std::cout<<"number[k]: "<<number[k]<<std::endl;
+//           // std::cout<<"number[k + i]: "<<number[k + i]<<std::endl;
+//           // std::cout<<"k: "<<k<<std::endl;
+//           // std::cout<<"k + i: "<<k + i<<std::endl;
+
+// // std::cout<<"--------------------------------------------------"<<std::endl;
+
+//           int tmp = number[k];
+//           number[k] = number[k + i];
+//           number[k + i] = tmp;
+//           std::cout<<"k: "<<k<<std::endl;
+//           std::cout<<"k + i: "<<k + i<<std::endl;
+//           std::cout<<"number[k]: "<<number[k]<<std::endl;
+//           std::cout<<"number[k + i]: "<<number[k + i]<<std::endl;
+//         }
+//       }
+//     }
+
+//     // std::cout<<"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"<<std::endl;
+//   } 
+// }
--- C++/Algorithm/Sort/Shell/main.cpp
+++ C++/Algorithm/Sort/Shell/main.cpp
@@ -3,7 +3,8 @@
 int main(void)
 {
   // int test[] = {1,45,78,54,545,245,45,152,4,251,55,478,564,45,889,56,5448,45,6,545,45,58,88};
-  int test[] = {9,8,7,6,5,4,3,2,1};
+  // int test[] = {9,8,7,6,5,4,3,2,1};
+  int test[] = {3,2,1};
 
   Shell tmp;
   tmp.sort(test, sizeof(test) / sizeof(int));
