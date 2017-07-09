9f65fce6d5645f3fa29f85d7550c3c98c791a924
JiangWeiGitHub <wei.jiang@winsuntech.cn>
2017-07-10 02:09:58 +0800

    modified:   Shell.cpp 	modified:   main.cpp

--- C++/Algorithm/Sort/Shell/Shell.cpp
+++ C++/Algorithm/Sort/Shell/Shell.cpp
@@ -32,56 +32,33 @@
 
 bool Shell::sort(int* number, int length)
 {
-  // int left = 0;
-  // if(length % 2 == 0)
-  // {
-  //   left = 0;
-  // }
-  // else
-  // {
-  //   left = 1;
-  // }
 
   for(int i = length / 2; i >= 1; i = i / 2)
   {
-    std::cout<<"i: "<<i<<std::endl;
-
     for(int j = 0; j < i; j ++)
     {
-      std::cout<<"j: "<<j<<std::endl;
-
       for(int k = j; k < length; k += i)
       {
-        std::cout<<"k: "<<k<<std::endl;
-
-        int stick = j;
-        while(stick < k)
+        if(k == j)
         {
-          if(number[stick] > number[k])
+          continue;
+        }
+
+        for(int l = j; l < length; l += i)
+        {
+          if(number[l] > number[k])
           {
-            int test = k
-            while(k > j)
+            int tmp = number[k];
+            while(l < k)
             {
-              
+              number[k] = number[k - i];
               k -= i;
             }
+
+            number[k] = tmp;
+
+            break;
           }
-
-
-
-
-          // // std::cout<<"stick: "<<stick<<std::endl;
-          // int test = j;
-          // while(number[stick] > number[test])
-          // {
-          //   int tmp = number[stick];
-          //   number[stick] = number[k];
-          //   number[k] = tmp;
-
-          //   test += i;
-          // }
-
-          stick += i;
         }
       }
     }
