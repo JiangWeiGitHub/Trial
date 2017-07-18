6e4c6a93c3e02dfb90f7c8d1f47bdd49f3fbbae5
JiangWeiGitHub <wei.jiang@winsuntech.cn>
2017-07-18 23:28:37 +0800

    modified:   calcCharCount.c

--- C/Homework/calcCharCount.c
+++ C/Homework/calcCharCount.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <assert.h>
+
 struct print
 {
   char nameList[26];
@@ -79,6 +81,47 @@ void calcCharCount(char *s)
     }
   }
 }
+
+// void calcCharCount(char *s)
+// {
+//   int i=0, j=0, max=0;
+//   int count[26]={0};
+//   int flag=0;
+//   int flag2=0;
+
+//   assert(s);
+//   for(i=0;s[i]!='\0';i++)
+//   {
+//     if(s[i]>='a' && s[i]<='z')
+//     {
+//       max=(++count[(int)s[i]-97])>max?count[(int)s[i]-97]:max;
+//     }
+//   }
+
+//   for(i=1;i<=max;i++)
+//   {
+//     for(j=0;j<25;j++)
+//     {
+//       if(count[j]==i)
+//       {
+//         printf("%c",(char)(j+97));
+//         flag=1;
+//       }
+//     }
+
+//     if(flag)
+//     {
+//       printf(" :%2d\n",i);
+//       flag=0;
+//       flag2=1;
+//     }
+//   }
+
+//   if(!flag2)
+//   {
+//     printf("No matched!\n");
+//   }
+// }
 
 void main(void)
 {
