bffe0de20959617bf718d6d0795a9f1984bb68f7
JiangWeiGitHub <wei.jiang@winsuntech.cn>
2017-08-17 00:25:31 +0800

    modified:   main.c

--- C/Homework/miniStack/main.c
+++ C/Homework/miniStack/main.c
@@ -7,6 +7,11 @@ struct stack
   int top;
 };
 
+void init(struct stack *name)
+{
+  name->top = 0;
+}
+
 int pop(struct stack *name)
 {
   if(name->top > 1)
@@ -22,7 +27,7 @@ int pop(struct stack *name)
 
 void push(struct stack *name, int number)
 {
-  if(name->top < 1023 && name->top > 0)
+  if(name->top < 1023 && name->top >= 0)
   {
     name->top += 1;
     (name->number)[name->top] = number;
@@ -35,15 +40,27 @@ void push(struct stack *name, int number)
 
 int getMini(struct stack *name)
 {
-  return (name->number)[name->top];
+  if(name->top > 0)
+  {
+    return (name->number)[name->top];
+  }
+  else
+  {
+    return 65535;
+  }
 }
 
-struct stack *one = (struct stack *)malloc(sizeof(struct stack));
-struct stack *two = (struct stack *)malloc(sizeof(struct stack));
+struct stack *one;
+struct stack *two;
 
 int main(void)
 {
-  int tmp[3] = {5,4,3};
+  one = (struct stack *)malloc(sizeof(struct stack));
+  two = (struct stack *)malloc(sizeof(struct stack));
+  init(one);
+  init(two);
+
+  int tmp[] = {5,4,3,6,8,9,2,14};
   int i;
   for(i = 0; i < (sizeof(tmp) / sizeof(tmp[0])); i ++)
   {
@@ -54,10 +71,57 @@ int main(void)
     }
   }
 
-  for(i = 0; i < (sizeof(tmp) / sizeof(tmp[0])); i ++)
+  printf("one: ");
+  for(i = 0; i <= one->top; i ++)
   {
-    printf("one: %d, two: %d\n", (one->number)[i], (two->number)[i] );
+    printf("%d ", (one->number)[i]);
   }
+  printf("\n");
+
+  printf("two: ");
+  for(i = 0; i <= two->top; i ++)
+  {
+    printf("%d ", (two->number)[i] );
+  }
+
+  printf("\n");
+
+  printf("******************************************\n");
+  
+  if( getMini(two) == (one->number)[one->top] )
+  {
+    pop(one);
+    pop(two);
+  }
+  else
+  {
+    pop(one);
+  }
+
+  if( getMini(two) == (one->number)[one->top] )
+  {
+    pop(one);
+    pop(two);
+  }
+  else
+  {
+    pop(one);
+  }
+
+  printf("one: ");
+  for(i = 0; i <= one->top; i ++)
+  {
+    printf("%d ", (one->number)[i]);
+  }
+  printf("\n");
+
+  printf("two: ");
+  for(i = 0; i <= two->top; i ++)
+  {
+    printf("%d ", (two->number)[i] );
+  }
+
+  printf("\n");
 
   return 0;
 }
