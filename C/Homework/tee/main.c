68ecaad23671c43603a896a813aaa0339bd7da8f
JiangWeiGitHub <wei.jiang@winsuntech.cn>
2017-08-19 23:43:40 +0800

    modified:   main.c

--- C/Homework/tee/main.c
+++ C/Homework/tee/main.c
@@ -1,3 +1,62 @@
+// #define _GNU_SOURCE
+// #include <fcntl.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+// #include <errno.h>
+// #include <limits.h>
+
+// int
+// main(int argc, char *argv[])
+// {
+//   int fd;
+//   int len, slen;
+
+//   if (argc != 2) {
+//     fprintf(stderr, "Usage: date |./a.out out.log | cat\n");
+//     exit(EXIT_FAILURE);
+//   }
+
+//   fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+//   if (fd == -1) {
+//     perror("open");
+//     exit(EXIT_FAILURE);
+//   }
+
+//   do
+//   {
+//     len = tee(STDIN_FILENO, STDOUT_FILENO,
+//                 INT_MAX, SPLICE_F_NONBLOCK);
+
+//     if (len < 0) {
+//       if (errno == EAGAIN)
+//         continue;
+//       perror("tee");
+//       exit(EXIT_FAILURE);
+//     }
+//     else
+//       if (len == 0)
+//         break;
+
+//     while (len > 0)
+//     {
+//       slen = splice(STDIN_FILENO, NULL, fd, NULL,
+//                       len, SPLICE_F_MOVE);
+//       if (slen < 0)
+//       {
+//         perror("splice");
+//         break;
+//       }
+//       len -= slen;
+//     }
+//   } while (1);
+
+//   close(fd);
+//   exit(EXIT_SUCCESS);
+// }
+
+/* using splice() & tee(), copy "./1.txt"'s contents to "./2.txt" and "./3.txt" at the same time*/
+
 #define _GNU_SOURCE
 #include <fcntl.h>
 #include <stdio.h>
@@ -6,53 +65,32 @@
 #include <errno.h>
 #include <limits.h>
 
-int
-main(int argc, char *argv[])
-{
-    int fd;
-    int len, slen;
+int main(){
+  int fd1 = open("./1.txt", O_RDONLY);
+  int fd2 = open("./2.txt", O_RDWR| O_CREAT | O_TRUNC, 0666);
+  int fd3 = open("./3.txt", O_RDWR| O_CREAT | O_TRUNC, 0666);
 
-    if (argc != 2) {
-        fprintf(stderr, "Usage: date |./a.out out.log | cat\n");
-        exit(EXIT_FAILURE);
-    }
+  int pipefd2[2];
+  int pipefd3[2];
 
-    fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (fd == -1) {
-        perror("open");
-        exit(EXIT_FAILURE);
-    }
+  pipe(pipefd2);
+  pipe(pipefd3);
 
-    do {
-        /*
-        * tee stdin to stdout.
-        */
-        len = tee(STDIN_FILENO, STDOUT_FILENO,
-                    INT_MAX, SPLICE_F_NONBLOCK);
+  splice(fd1, NULL, pipefd2[1], NULL, 10086, SPLICE_F_MORE);
 
-        if (len < 0) {
-            if (errno == EAGAIN)
-                continue;
-            perror("tee");
-            exit(EXIT_FAILURE);
-        } else
-            if (len == 0)
-                break;
+  tee(pipefd2[0], pipefd3[1], 10086, SPLICE_F_NONBLOCK);
 
-        /*
-        * Consume stdin by splicing it to a file.
-        */
-        while (len > 0) {
-            slen = splice(STDIN_FILENO, NULL, fd, NULL,
-                            len, SPLICE_F_MOVE);
-            if (slen < 0) {
-                perror("splice");
-                break;
-            }
-            len -= slen;
-        }
-    } while (1);
+  splice(pipefd2[0], NULL, fd2, NULL, 10086, SPLICE_F_MORE);
 
-    close(fd);
-    exit(EXIT_SUCCESS);
+  splice(pipefd3[0], NULL, fd3, NULL, 10086, SPLICE_F_MORE);
+
+  close(fd1);
+  close(fd2);
+  close(fd3);
+  close(pipefd2[0]);
+  close(pipefd2[1]);
+  close(pipefd3[0]);
+  close(pipefd3[1]);
+
+  return 0;
 }
