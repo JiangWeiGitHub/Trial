9c998735e693347293f56f73528b0d8420008d0c
JiangWeiGitHub <wei.jiang@winsuntech.cn>
2017-09-26 23:47:54 +0800

    modified:   main.c

--- C/Homework/weightPass/main.c
+++ C/Homework/weightPass/main.c
@@ -89,30 +89,30 @@ void main()
 {
   struct data testData1, testData2, testData3, testData4, testData5;
 
-	// 12 45 8 74 1
+  // 12 45 8 74 1
 
-	testData1.first = 12;
-	testData1.last = 45;
+  testData1.first = 12;
+  testData1.last = 45;
 
-	testData2.first = 8;
-	testData2.last = 74;
+  testData2.first = 8;
+  testData2.last = 74;
 
-	testData3.first = 45;
-	testData3.last = 8;
+  testData3.first = 45;
+  testData3.last = 8;
 
-	testData4.first = 74;
-	testData4.last = 1;
+  testData4.first = 74;
+  testData4.last = 1;
 
-	testData5.first = 12;
-	testData5.last = 8;
+  testData5.first = 12;
+  testData5.last = 8;
 
-	int testRound = 5;
+  int testRound = 5;
 
-	struct data testData[5] = {testData1, testData2, testData3, testData4, testData5};
+  struct data testData[5] = {testData1, testData2, testData3, testData4, testData5};
 
-	struct data req;
-	req.first = 12;
-	req.last = 1;
+  struct data req;
+  req.first = 12;
+  req.last = 1;
 
   struct link list[MAX];
 
