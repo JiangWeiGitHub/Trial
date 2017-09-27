f13584612bc1789d974e5aec83f4382ecd51b7a1
JiangWeiGitHub <wei.jiang@winsuntech.cn>
2017-09-27 23:08:56 +0800

    modified:   main.c

--- C/Homework/weightPass/main.c
+++ C/Homework/weightPass/main.c
@@ -73,59 +73,59 @@
 // 	}
 // }
 
-struct data
-{
-	int first;
-	int last;
-};
+// struct data
+// {
+// 	int first;
+// 	int last;
+// };
 
-struct link
-{
-	int name;
-	struct link* next;
-};
+// struct link
+// {
+// 	int name;
+// 	struct link* next;
+// };
 
-void main()
-{
-  struct data testData1, testData2, testData3, testData4, testData5;
+// void main()
+// {
+//   struct data testData1, testData2, testData3, testData4, testData5;
 
-  // 12 45 8 74 1
+//   // 12 45 8 74 1
 
-  testData1.first = 12;
-  testData1.last = 45;
+//   testData1.first = 12;
+//   testData1.last = 45;
 
-  testData2.first = 8;
-  testData2.last = 74;
+//   testData2.first = 8;
+//   testData2.last = 74;
 
-  testData3.first = 45;
-  testData3.last = 8;
+//   testData3.first = 45;
+//   testData3.last = 8;
 
-  testData4.first = 74;
-  testData4.last = 1;
+//   testData4.first = 74;
+//   testData4.last = 1;
 
-  testData5.first = 12;
-  testData5.last = 8;
+//   testData5.first = 12;
+//   testData5.last = 8;
 
-  int testRound = 5;
+//   int testRound = 5;
 
-  struct data testData[5] = {testData1, testData2, testData3, testData4, testData5};
+//   struct data testData[5] = {testData1, testData2, testData3, testData4, testData5};
 
-  struct data req;
-  req.first = 12;
-  req.last = 1;
+//   struct data req;
+//   req.first = 12;
+//   req.last = 1;
 
-  struct link list[MAX];
+//   struct link list[MAX];
 
-  int i;
-  for(i = 0; i < sizeof(testData); i++)
-  {
-    if(list[testData[i].first].name != testData[i].first)
-    {
-      list[testData[i].first].name = testData[i].first; 
-    }
-    else
-    {
+//   int i;
+//   for(i = 0; i < sizeof(testData); i++)
+//   {
+//     if(list[testData[i].first].name != testData[i].first)
+//     {
+//       list[testData[i].first].name = testData[i].first; 
+//     }
+//     else
+//     {
       
-    }
-  }
-}
+//     }
+//   }
+// }
