1b29142e450db723d03d4c4d9dbf0c257e3be571
JiangWeiGitHub <wei.jiang@winsuntech.cn>
2017-09-29 23:54:35 +0800

    modified:   main.c

--- C/Homework/sumAAndB/main.c
+++ C/Homework/sumAAndB/main.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define MAX 1024
+// #define MAX 1024
+#define MAX 8
 
 void main(void)
 {
   char data[MAX/8] = {0};
-  int testData[MAX] = {1,2,5,8,6,5,445,5,85,5,258,2,532,52,5,266,35,\
-  326,36,36,226,62,85,48,457,54,454,54,54,878,54,54,54,5,54,54,54,};
+  // int testData[MAX] = {1,2,5,8,6,5,445,5,85,5,258,2,532,52,5,266,35,\
+  // 326,36,36,226,62,85,48,457,54,454,54,54,878,54,54,54,5,54,54,54,};
+  int testData[MAX] = {1,2,5,8,6,5,445,5};
 
   int i;
   for(i = 0; i < MAX; i++)
   {
-    data = data | 1 << testData[i];
+    data = data[testData[i] % 8] | 1 << testData[i];
   }
+
+  // printf("data: %x\n", testData[0]);
 
 }
