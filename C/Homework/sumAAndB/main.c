9abc41f0df836c57a486981d283ef5baa70be5f4
JiangWeiGitHub <wei.jiang@winsuntech.cn>
2017-10-01 23:44:58 +0800

    modified:   main.c

--- C/Homework/sumAAndB/main.c
+++ C/Homework/sumAAndB/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 // #define MAX 1024
-#define MAX 8
+#define MAX 16
 
 void main(void)
 {
@@ -10,7 +10,8 @@ void main(void)
   // int testData[MAX] = {1,2,5,8,6,5,445,5,85,5,258,2,532,52,5,266,35,\
   // 326,36,36,226,62,85,48,457,54,454,54,54,878,54,54,54,5,54,54,54,};
   // int testData[MAX] = {1,2,5,7,6,5,3,5};
-  int testData[MAX] = {1,2,3,4,5,6,7,0};
+  // int testData[MAX] = {1,2,3,4,5,6,7,0};
+  int testData[MAX] = {1,2,3,4,5,6,7,0,8,9,10,11,13,12,14,15};
 
   int i;
   for(i = 0; i < MAX; i++)
